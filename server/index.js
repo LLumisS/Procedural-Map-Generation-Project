02b9a081a04877fc524c9604e2dfc716a29841dd
LLumisS <vasa16671@gmail.com>
2023-05-25 19:22:16 +0300

    initial commit (server)

---
+++ server/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const sequelize = require('./db');
+const models = require('./models/models');
+const cors = require('cors');
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: "Working."});
+});
+
+const start = async() => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync()
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+start();
---
+++ server/models/models.js
@@ -0,0 +1,66 @@
+const sequelize = require('../db');
+const {DataTypes} = require('sequelize');
+
+const User = sequelize.define('user', {
+    id:       {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    login:    {type: DataTypes.STRING, unique: true},
+    password: {type: DataTypes.STRING}
+});
+
+const Map = sequelize.define('map', {
+    id:     {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    shared: {type: DataTypes.BOOLEAN, defaultValue: false},
+
+});
+
+const Matrix = sequelize.define('matrix', {
+    id:          {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    physical:    {type: DataTypes.STRING, unique: true},
+    moisture:    {type: DataTypes.STRING, unique: true},
+    temperature: {type: DataTypes.STRING, unique: true},
+    bioms:       {type: DataTypes.STRING, unique: true}
+});
+
+const Mark = sequelize.define('mark', {
+    id:    {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    value: {type: DataTypes.INTEGER, allowNull: false},
+});
+
+const Rating = sequelize.define('rating', {
+    id:    {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    value: {type: DataTypes.DOUBLE},
+});
+
+const Save = sequelize.define('save', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+});
+
+User.hasMany(Map);
+Map.belongsTo(User);
+
+User.hasMany(Mark);
+Mark.belongsTo(User);
+
+User.hasMany(Save);
+Save.belongsTo(User);
+
+Map.hasMany(Mark);
+Mark.belongsTo(Map);
+
+Map.hasMany(Save);
+Save.belongsTo(Map);
+
+Map.hasOne(Matrix);
+Matrix.belongsTo(Map);
+
+Rating.hasOne(Map);
+Map.belongsTo(Rating);
+
+module.exports = {
+    User,
+    Map,
+    Matrix,
+    Mark,
+    Rating,
+    Save
+}
