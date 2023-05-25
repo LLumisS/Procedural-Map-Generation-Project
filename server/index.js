847b23561095eb8a736b9a5eda7902f19766c943
LLumisS <vasa16671@gmail.com>
2023-05-25 23:47:44 +0300

    get img

--- server/index.js
+++ server/index.js
@@ -6,12 +6,14 @@ const cors = require('cors');
 const fileUpload = require('express-fileupload');
 const router = require('./routes/index');
 const errorHandler = require('./middleware/ErrorHandlerMiddleware');
+const path = require('path');
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'static')));
 app.use(fileUpload({}));
 ;app.use('/api', router);
 
