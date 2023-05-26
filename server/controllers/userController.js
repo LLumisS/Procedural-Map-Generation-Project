b04b4f0de5dd386b28d5e208699306ff1f04b2b8
LLumisS <vasa16671@gmail.com>
2023-05-26 18:21:15 +0300

    user updated

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,28 @@
+const { User } = require('../models/models');
 const ApiError = require('../error/ApiError');
 
 class UserController {
-    async registration(req, res) {
+    async get (req, res, next) {
+        try {
+            const users = await User.findAll();
+            return res.json({ users });
+        } catch (e) {
+            next(ApiError.badRequest(e.message));
+        }
+    }
 
+    async registration(req, res, next) {
+        try {
+        const { login, password } = req.body;
+        if(!login || !password) {
+            return next(ApiError.badRequest('User data expected'));
+        }
+
+        const user = await User.create({ login: login, password: password });
+        return res.json(user);
+        } catch (e) {
+            next(ApiError.badRequest(e.message));
+        }
     }
 
     async login(req, res) {
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -2,6 +2,8 @@ const Router = require('express');
 const router = new Router();
 const userController = require('../controllers/userController');
 
+router.get('/', userController.get);
+router.post('/', userController.registration);
 router.post('/registration', userController.registration);
 router.post('/login', userController.login);
 router.get('/auth', userController.check);
