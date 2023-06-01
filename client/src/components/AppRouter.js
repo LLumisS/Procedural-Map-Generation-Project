0ab425bf1704e9c7e918ad2a7a368c5bab61a3b7
LLumisS <vasa16671@gmail.com>
2023-06-01 14:34:05 +0300

    redirect added

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Routes, Route, Redirect } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { authRoutes, publicRoutes } from '../routes';
+import { HOMEPAGE_ROUTE } from '../utils/consts';
 
 const AppRouter = () => {
   const isAuth = false;
   return (
     <Routes>
-      {isAuth === true && authRoutes.map(({ path, Component }) => (
+      {isAuth && authRoutes.map(({ path, Component }) => (
         <Route key={path} path={path} element={<Component />} exact/>
       ))}
       {publicRoutes.map(({ path, Component }) => (
         <Route key={path} path={path} element={<Component />} exact/>
       ))}
+      <Route path="*" element={<Navigate to={HOMEPAGE_ROUTE} />} exact/>
     </Routes>
   );
 };
