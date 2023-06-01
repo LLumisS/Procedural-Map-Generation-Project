6288dfab5804de05a5b277dd774a9c4a9e3b5d90
LLumisS <vasa16671@gmail.com>
2023-06-02 00:01:20 +0300

    generator page update

--- client/src/pages/Generator.js
+++ client/src/pages/Generator.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { useRef, useEffect } from 'react';
 import tableImage from '../table.png';
-import { ButtonGroup, Button, Form, Container, Card } from 'react-bootstrap';
+import { Button, Form, Container, Card } from 'react-bootstrap';
+import { observer } from 'mobx-react-lite';
+
+const WIDTH = 400;
+const HEIGHT = 400;
 
 const CanvasComponent = () => {
   const canvasRef = useRef(null);
@@ -14,33 +18,83 @@ const CanvasComponent = () => {
     context.fillRect(0, 0, canvas.width, canvas.height);
   }, []);
 
-  return <canvas ref={canvasRef} width={400} height={400} />;
+  return <canvas ref={canvasRef} width={WIDTH} height={HEIGHT} />;
 };
 
 const Generator = () => (
   <Container
     className="d-flex justify-content-center align-items-center"
     style={{ height: window.innerHeight - 54 }}>
-    <Card style={{ width: 270 }} className="p-4">
-      <h4 className="mb-3" style={{ textAlign: 'center' }}>Settings</h4>
-      <h5 className="mb-2">Filters:</h5>
-      <Form style={{ marginLeft: '10px' }}>
-        {['Default', 'Physical', 'Moisture', 'Temperature'].map(name =>
-          (
-            <div key={`${name}-radio`} className="mb-2">
-              <Form.Check
-                type='radio'
-                name='group1'
-                id={`${name}-radio`}
-                label={`${name}`}
-                defaultChecked={name === 'Default'}
-              />
-            </div>
-          )
-        )}
-      </Form>
-    </Card>
+    <div>
+      <h3 style={{ marginTop: '20px' }}
+        className="d-flex justify-content-center"
+      >Procedural Map Generation</h3>
+      <div style={{ display: 'flex',  flexDirection: 'row'  }}
+        className="d-flex justify-content-center">
+        <div style={{ flex: '0 0 400px',
+          marginLeft: '20px',
+          marginTop: '5px' }}>
+          <CanvasComponent />
+        </div>
+        <Card style={{
+          width: 270,
+          height: HEIGHT,
+          marginLeft: '30px',
+          marginTop: '5px' }} className="p-4">
+          <h4 className="mb-3" style={{ textAlign: 'center' }}>Settings</h4>
+          <h5 className="mb-2">Filters:</h5>
+          <Form style={{ marginLeft: '10px' }}>
+            {['Default', 'Physical', 'Moisture', 'Temperature'].map(name =>
+              (
+                <div key={`${name}-radio`} className="mb-2">
+                  <Form.Check
+                    type='radio'
+                    name='group1'
+                    id={`${name}-radio`}
+                    label={`${name}`}
+                    defaultChecked={name === 'Default'}
+                  />
+                </div>
+              )
+            )}
+          </Form>
+          <div className="d-flex justify-content-center">
+            <Button className="mt-2" style={{ width: '150px' }}>Apply</Button>
+          </div>
+          <div className="d-flex justify-content-center">
+            <Button className="mt-5" style={{ width: '150px' }}>Create</Button>
+          </div>
+        </Card>
+      </div>
+    </div>
+    {/*     <div style={{ flex: '1' }}>
+      <img
+        src={tableImage}
+        alt="Table"
+        style={{
+          marginTop: '10px',
+          marginLeft: '10px',
+          backgroundColor: 'floralwhite',
+          width: '395px',
+          height: '360px'
+        }}
+      />
+    </div> */}
   </Container>
 );
 
 export default Generator;
+/*
+<div style={{ flex: '1' }}>
+          <img
+            src={tableImage}
+            alt="Table"
+            style={{
+              marginTop: '10px',
+              marginLeft: '10px',
+              backgroundColor: 'floralwhite',
+              width: '395px',
+              height: '360px'
+            }}
+          />
+        </div> */
