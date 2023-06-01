bd5daa5270d966142def5d2043348731fc2d5b8f
LLumisS <vasa16671@gmail.com>
2023-06-01 13:52:20 +0300

    consts

---
+++ client/src/utils/consts.js
@@ -0,0 +1,6 @@
+export const ADMIN_ROUTE = '/admin';
+export const REGISTRATION_ROUTE = '/registration';
+export const LOGIN_ROUTE = '/login';
+export const SAVES_ROUTE = '/saves';
+export const HOMEPAGE_ROUTE = '/homepage';
+export const GENERATOR_ROUTE = '/generator';
