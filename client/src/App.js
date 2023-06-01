6bf66cd48785b8ef04a74ea09dd53b9f851e8530
LLumisS <vasa16671@gmail.com>
2023-06-01 13:52:38 +0300

    app router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import AppRouter from './components/AppRouter';
 
 function App() {
   return (
-    <div className="App">
-      SERVER
-    </div>
+    <BrowserRouter>
+      <AppRouter/>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/components/AppRouter.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+const AppRouter = () => (
+  <div></div>
+);
+
+export default AppRouter;
