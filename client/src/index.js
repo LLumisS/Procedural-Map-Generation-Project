cb1a916c1f71f6ea2281bb655581ba77ab923c58
LLumisS <vasa16671@gmail.com>
2023-06-01 14:47:59 +0300

    auth

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import UserStore from './store/UserStore';
+
+export const Context = createContext(null);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Context.Provider value={{
+    user: new UserStore(),
+  }}>
     <App />
-  </React.StrictMode>
+  </Context.Provider>,
 );
