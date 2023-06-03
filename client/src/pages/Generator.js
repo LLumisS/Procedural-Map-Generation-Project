f63a48f825b862abd2fb5636d1adf0495d2f140b
LLumisS <vasa16671@gmail.com>
2023-06-04 00:56:05 +0300

    generator page refactoring

--- client/src/pages/Generator.js
+++ client/src/pages/Generator.js
@@ -28,64 +28,88 @@ const buttonStyle = {
   borderColor: 'black'
 };
 
+const FiltersForm = () => {
+  const filters = ['Default', 'Physical', 'Moisture', 'Temperature'];
+
+  return (
+    <Form style={{ marginLeft: '10px' }}>
+      {filters.map(name => (
+        <div key={`${name}-radio`} className="mb-2">
+          <Form.Check
+            type='radio'
+            name='group1'
+            id={`${name}-radio`}
+            label={`${name}`}
+            defaultChecked={name === 'Default'}
+          />
+        </div>
+      ))}
+    </Form>
+  );
+};
+
+const MapActions = () => (
+  <div className="d-flex justify-content-center">
+    <Button className="mt-2"
+      style={{
+        width: '135px',
+        backgroundColor: '#dbdbdb',
+        color: 'black',
+        borderColor: 'black'
+      }}>Apply</Button>
+  </div>
+);
+
+const CreateButton = () => (
+  <div className="d-flex justify-content-center">
+    <Button className="mt-5" style={{ width: '180px', height: '47px' }}>
+      Create
+    </Button>
+  </div>
+);
+
 const Generator = () => (
-  <Container
-    style={{ height: window.innerHeight - 54 }}>
+  <Container style={{ height: window.innerHeight - 54 }}>
     <Card className="p-3" style={{ marginTop: '20px' }}>
       <div>
-        <h3 className="d-flex justify-content-center">
-        Procedural Map Generation</h3>
-        <div style={{ display: 'flex',  flexDirection: 'row'  }}
-          className="d-flex justify-content-center">
-          <div style={{ flex: '0 0 400px',
-            marginTop: '5px', border: '2px solid black' }}>
-            <CanvasComponent />
+        <h3 className="d-flex justify-content-center"
+        >Procedural Map Generation</h3>
+        <div style={{
+          display: 'flex',
+          flexDirection: 'row'
+        }} className="d-flex justify-content-center">
+          <div style={{
+            flex: '0 0 400px',
+            marginTop: '5px',
+            border: '2px solid black'
+          }}><CanvasComponent />
           </div>
           <Card style={{
             width: 270,
             height: `${HEIGHT + 3}px`,
             marginLeft: '30px',
             marginTop: '5px',
-            backgroundColor: '#f7f7f7', }} className="p-4">
+            backgroundColor: '#f7f7f7'
+          }} className="p-4">
             <h4 className="mb-3" style={{ textAlign: 'center' }}>Settings</h4>
             <h5 className="mb-2">Filters:</h5>
-            <Form style={{ marginLeft: '10px' }}>
-              {['Default', 'Physical', 'Moisture', 'Temperature'].map(name =>
-                (
-                  <div key={`${name}-radio`} className="mb-2">
-                    <Form.Check
-                      type='radio'
-                      name='group1'
-                      id={`${name}-radio`}
-                      label={`${name}`}
-                      defaultChecked={name === 'Default'}
-                    />
-                  </div>
-                )
-              )}
-            </Form>
-            <div className="d-flex justify-content-center">
-              <Button className="mt-2"
-                style={{ width: '135px',
-                  backgroundColor: '#dbdbdb',
-                  color: 'black',
-                  borderColor: 'black' }}>Apply</Button>
-            </div>
-            <div className="d-flex justify-content-center">
-              <Button className="mt-5"
-                style={{ width: '180px', height: '47px' }}>Create</Button>
-            </div>
+            <FiltersForm />
+            <MapActions />
+            <CreateButton />
           </Card>
         </div>
         <div className="d-flex justify-content-center">
-          <ButtonGroup
-            style={{ marginTop: '15px', width: `${WIDTH + 30 + 270}px` }}>
+          <ButtonGroup style={{
+            marginTop: '15px',
+            width: `${WIDTH + 30 + 270}px`
+          }}>
             <Button style={buttonStyle}>Download</Button>
             <Button style={buttonStyle}>Save</Button>
             <Button style={buttonStyle}>Share</Button>
           </ButtonGroup>
         </div>
-      </div></Card>
+      </div>
+    </Card>
     <div style={{ flex: '1' }} className="d-flex justify-content-center">
       <img
         src={tableImage}
